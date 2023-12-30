1fe59c8dc150e7a07580d30bfb7573b5f2858c3b
Ismail ech-chaouny <chaouniismail4@gmail.com>
2023-12-30 22:46:12 +0100

    add Navbar, Home page, 3D island

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,42 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-12 h-12 rounded-lg bg-white flex items-center justify-center font-semibold text-[25px] shadow-md"
+      >
+        <p className="blue-gradient_text">IE</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
